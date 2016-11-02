233de05a6695fccba42688c9a67c7c681a544ff1
Mario Truyens <mario@unicase.be>
2016-11-02 16:40:06 +0100

    JPA test

--- FeeService/src/main/java/com/abnamro/poc/feeservice/Contract.java
+++ FeeService/src/main/java/com/abnamro/poc/feeservice/Contract.java
@@ -2,7 +2,10 @@ package com.abnamro.poc.feeservice;
 
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlElement;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
 @XmlRootElement
 public class Contract {
 	int id;
@@ -15,52 +18,53 @@ public class Contract {
 		this.id = id;
 	}
 
-        public int getId() {
-            return this.id;
-        }
+	@Id
+    public int getId() {
+        return this.id;
+    }
 
 	@XmlElement
 	public void setVersion(int version) {
 		this.version = version;
 	}
 
-        public int getVersion() {
-            return this.version;
-        }
+    public int getVersion() {
+        return this.version;
+    }
 	
 	@XmlElement
 	public void setSource(String source) {
 		this.source = source;
 	}
 
-        public String getSource() {
-            return this.source;
-        }
+    public String getSource() {
+        return this.source;
+    }
 	
 	@XmlElement
 	public void setFamily(String family) {
 		this.family = family;
 	}
 
-        public String getFamily() {
-            return this.family;
-        }
+    public String getFamily() {
+        return this.family;
+    }
 	
 	@XmlElement
 	public void setGroup(String group) {
 		this.group = group;
 	}
 
-        public String getGroup() {
-            return this.group;
-        }
+    public String getGroup() {
+        return this.group;
+    }
 	
 	@XmlElement
 	public void setType(String type) {
 		this.type = type;
 	}
 
-        public String getType() {
-            return this.type;
-        }
+    public String getType() {
+        return this.type;
+    }
 }
