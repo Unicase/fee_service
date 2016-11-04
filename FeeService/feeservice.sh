3cbac2687907a27504627b44a254f2f43db1e2aa
Mario Truyens <mario@unicase.be>
2016-11-04 14:16:40 +0100

    move from .properties to .yml

--- FeeService/feeservice.sh
+++ FeeService/feeservice.sh
@@ -1,1 +1,4 @@
+cd /opt/feeservice/FeeService
+rm /var/log/feeservice/*
+export SPRING_PROFILES_ACTIVE=poc
 nohup mvn compile exec:java -Dexec.mainClass=com.abnamro.poc.feeservice.FeeService >/var/log/feeservice/feeservice.out 2>&1 &
