6522e8848fdd6965977aee39bc9227e9cda006fd
Mario Truyens <mario@unicase.be>
2016-11-04 14:53:15 +0100

    added maven clean

--- FeeService/feeservice.sh
+++ FeeService/feeservice.sh
@@ -1,4 +1,4 @@
 cd /opt/feeservice/FeeService
 rm /var/log/feeservice/*
 export SPRING_PROFILES_ACTIVE=poc
-nohup mvn compile exec:java -Dexec.mainClass=com.abnamro.poc.feeservice.FeeService >/var/log/feeservice/feeservice.out 2>&1 &
+nohup mvn clean:clean compile exec:java -Dexec.mainClass=com.abnamro.poc.feeservice.FeeService >/var/log/feeservice/feeservice.out 2>&1 &
